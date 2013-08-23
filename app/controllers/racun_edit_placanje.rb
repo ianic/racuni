57edd6717baf7d5b84088ebe0d3ca2afe60905c3
ianic <igor.anic@gmail.com>
2013-08-23 19:17:46 +0200

    Rendani rjs template je imao krivo postavljeni content type pa se nije izvrsio na klijentu treba biti text/javascript.

--- app/controllers/racun_edit_placanje.rb
+++ app/controllers/racun_edit_placanje.rb
@@ -1,30 +1,32 @@
 module RacunEditPlacanje
-  
+
   def add_placanje
     saved = @racun.placanja << (@racun_placanje = @racun.placanja.new(params[:racun_placanje]))
     @racun.placanja(true) if !saved
     @racun_placanje = nil if saved
     nova_uplata if saved
+    headers["Content-Type"] = "text/javascript; charset=utf-8"
     render :partial => "racun_placanje/update", :locals => {:update_racun_row => saved, :close_editor => saved}
   end
-  
+
   def delete_placanje
     @racun.placanja.delete(@racun.placanja.find(params[:placanje_id]))
     nova_uplata
+    headers["Content-Type"] = "text/javascript; charset=utf-8"
     render :partial => "racun_placanje/update", :locals => {:update_racun_row => true, :close_editor => false}
   end
-  
+
   def edit_placanje
     @title = naziv_dokumenta(@racun)
     nova_uplata
     render :partial => "racun/placanje"
   end
-  
+
   def nova_uplata
     return if @racun.placen
     @racun_placanje = @racun.placanja.new(
-      :iznos => @racun.za_naplatu, 
-      :datum => -1.days.from_now, 
+      :iznos => @racun.za_naplatu,
+      :datum => -1.days.from_now,
       :dokument =>  @user.zadnji_izvod)
   end
 
