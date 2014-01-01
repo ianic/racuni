124a99a85e0a8b49ce1243fa0265b2eb9129c5f0
Igor Anic <ianic@inet.hr>
2014-01-01 12:10:52 +0100

    jos jedan css treba biti precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Racuni::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( racun_ispis.css )
+  config.assets.precompile += %w( racun_ispis.css racun/default.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
