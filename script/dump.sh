89f841650ffe91e0bfac76d129c2a8e9df6f79f8
Igor Anic <ianic@inet.hr>
2020-12-19 20:42:29 +0100

    Sync backups to S3

--- script/dump.sh
+++ script/dump.sh
@@ -5,3 +5,5 @@ filename="$dir/racuni-$(date +%Y-%m-%d-%H-%M).bkp.gz"
 echo "$(date) backing up racuni to $filename"
 mysqldump -u root --password=Dsalkjm! racuni | gzip >$filename
 find $dir -type f -mtime +30 -exec rm {} \;
+
+/usr/local/bin/s3cmd sync $dir s3://$DUMPS_BUCKET
