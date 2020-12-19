30e3a2f72d5bc94ae9f6a6e0323d7a448f561a47
Igor Anic <ianic@inet.hr>
2020-12-19 19:10:46 +0100

    Produziti vrijeme cuvanja backupa

--- script/dump.sh
+++ script/dump.sh
@@ -4,4 +4,4 @@ dir=/var/apps/backup
 filename="$dir/racuni-$(date +%Y-%m-%d-%H-%M).bkp.gz"
 echo "$(date) backing up racuni to $filename"
 mysqldump -u root --password=Dsalkjm! racuni | gzip >$filename
-find $dir -type f -mtime +7 -exec rm {} \;
+find $dir -type f -mtime +30 -exec rm {} \;
